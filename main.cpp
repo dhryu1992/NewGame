2cb9361dec40e697f119adf4aaed4c77a794c2fb
dhryu1992 <sscross0727@gmail.com>
2020-11-20 19:37:46 +0900

    backup before reinstall Ubuntu

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
-#include "IntEnergy.h"
+/*#include "IntEnergy.h"
 #include "Weapon.h"
 #include "Saber.h"
 #include "Archer.h"
 #include "Caster.h"
 #include "Berserker.h"
+*/
+#include <iostream>
+#include <cstring>
+
+using namespace std;
 
 int main() {
-	Saber saber;
-	Archer archer;
-	Caster caster;
-	Berserker berserker;
+	char My_Name[20];
+	int size = 0;
+	size = sizeof My_Name[20];
+	cout << "Welcome to ShuLand!" << endl;
+	cout << "Enter your name: " << endl;
+	cin >> My_Name;
+	char* select = My_Name;
+	
+	//cout << "select: " << select << endl;
+	while(sizeof(select) {
+		cout <<"Error! Enter one more time!" << endl;
+	}
+	cout << "your name is " << My_Name << "." << endl;
+	return 0;
 }
